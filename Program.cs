3209aa4cda7d0d66523e2bdd13899da36074e38a
Gibran Shah <gshah.hab@outlook.com>
2020-01-23 21:51:44 -0700

    Grabbing args

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace Image2Excel
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string imageFile = "";
+            string excelFilename = "";
+
+            switch (args.Length) {
+                case 0:
+                    Console.WriteLine("Please supply an image file name and an optional Excel file name.");
+                    Console.WriteLine("Example: dotnet run [imagefile.ext [excelfile.xls]]");
+                    return;
+                case 1:
+                    imageFile = args[0];
+                    break;
+                case 2:
+                default:
+                    imageFile = args[0];
+                    excelFilename = args[1];
+                    break;
+            }
         }
     }
 }
